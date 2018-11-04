61f2429367979833f72ba36615144d6fca95ac59
superpeppa228 <43078376+superpeppa228@users.noreply.github.com>
2018-11-04 19:30:37 +0300

    Initial commit

---
+++ JS/javascript.js
@@ -0,0 +1,17 @@
+
+let tc = prompt ("Введите температуру в градусах Цельсия  ");
+let tf = tc*1.8 + 32;
+alert(tf);
+
+
+/*
+let name="Василий";
+let admin = name;
+alert(admin);
+*/
+
+/*
+//На экране будет 1000108//
+var a=1000+"108"
+alert (a);
+*/
